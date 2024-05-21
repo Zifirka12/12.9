f1e0663383096c315ad64ce21d8066ce98c7e775
Zifirka <Ilaafanasef21@gmail.com>
2024-05-21 20:35:31 +0300

    13.1.8 забыл к тестам написать док стринги , теперь есть

--- tests/test_CSV_XLSX.py
+++ tests/test_CSV_XLSX.py
@@ -7,6 +7,12 @@ from src.CSV_XLSX import read_transactions_csv, read_transactions_xlsx
 
 @patch("csv.DictReader")
 def test_read_from_csv(mock_csv_reader: Any) -> None:
+    """
+        Тестирование функционала чтения транзакций из CSV файла.
+
+        Args:
+            mock_csv_reader (Any): Мок-объект для имитации csv.DictReader.
+        """
     mock_csv_reader.return_value = iter(
         [{"Date": "2022-01-01", "Amount": "100.00"}, {"Date": "2022-02-01", "Amount": "200.00"}]
     )
@@ -17,6 +23,12 @@ def test_read_from_csv(mock_csv_reader: Any) -> None:
 
 @patch("pandas.read_excel")
 def test_read_from_xlsx(mock_read_excel: Any) -> None:
+    """
+    Тестирование функционала чтения транзакций из Excel файла.
+
+    Args:
+        mock_read_excel (Any): Мок-объект для имитации pandas.read_excel.
+    """
     mock_read_excel.return_value = pd.DataFrame({"Date": ["2022-01-01", "2022-02-01"], "Amount": [100.00, 200.00]})
     result = read_transactions_xlsx(r"C:\Users\User\pythonProject5.1(0)\data\transactions_excel.xlsx")
     expected_result = [{"Date": "2022-01-01", "Amount": 100.00}, {"Date": "2022-02-01", "Amount": 200.00}]
